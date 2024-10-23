4cba02958f3b06d3c3805c80d57f35e2a68e248c
Thiago Elias <thiagoelias99@gmail.com>
2024-10-23 13:50:46 -0300

    style: :art: Customize components

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/Components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/Components/ui/card';
 import { H2 } from '@/Components/ui/typography';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
@@ -22,6 +23,13 @@ export default function Dashboard() {
                 </CardHeader>
                 <CardContent>
                     <H2>Bem vindo de volta, {user.name}!</H2>
+                    <Button >Hello</Button>
+                    <Button variant="destructive">Hello</Button>
+                    <Button variant="secondary">Hello</Button>
+                    <Button variant="ghost">Hello</Button>
+                    <Button variant="outline">Hello</Button>
+                    <Button variant="muted">Hello</Button>
+                    <Button variant="tertiary">Tert</Button>
                 </CardContent>
             </Card>
         </AuthenticatedLayout>
